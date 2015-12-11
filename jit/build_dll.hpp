5677d85326cdf9fd3e2f4fc6d5219cd876bcfa75
173210 <root.3.173210@live.com>
2015-12-11 13:27:59 +0900

    Ignore intermediate files

--- jit/build_dll.hpp
+++
@@ -1,7 +0,0 @@
-
-#define PREFIX "/home/wqin/work/mipsim/build"
-#define SED "/bin/sed"
-#define CXX "g++"
-#define FP_OPTION "  -msse2 -mfpmath=sse"
-
-#define MORE_OPTION " -O -fpic -shared -pipe -fomit-frame-pointer"
